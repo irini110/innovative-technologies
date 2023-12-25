d9cb625a36c5f52e9f2b10cbff168c7ba91abb3e
irini110 <146884582+irini110@users.noreply.github.com>
2023-12-26 01:19:17 +0300

    Update index.js

--- week10/index.js
+++ week10/index.js
@@ -21,7 +21,7 @@ window.addEventListener("DOMContentLoaded", async () => {
 
 	const loader = new THREE.TextureLoader();
 
-	const texture = loader.load( '../assets/mymask-removebg-preview.png' );
+	const texture = loader.load( '../assets/mask.png' );
 	faceMesh.material.map = texture;
 	faceMesh.material.transparent = true;
 	faceMesh.material.needsUpdate = true;
